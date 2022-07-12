07911311a51e0acdf80ff319d93fd1ae2958e8d7
Tarushi Patidar <tarushipatidar123@gmail.com>
2022-07-12 20:04:54 +0530

    Added seed data for application test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+ShortUrlData.create(real_url: 'https://www.google.com/webhp?hl=en&sa=X&ved=0ahUKEwiI_LzZyPP4AhXDmFYBHaexDqwQPAgI', short_url: 'abcde', expiry_date: DateTime.now + 30.days) # use in 30 days
+ShortUrlData.create(real_url: 'https://paper.dropbox.com/doc/Build-a-URL-shortener-BdG2JwuLz5jG4ke1kf4Ye', short_url: 'short', expiry_date: DateTime.now - 50.days) # expired
