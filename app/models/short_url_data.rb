ebd3af9b90b731012d0a9766be786abcdbd237a4
Tarushi Patidar <tarushipatidar123@gmail.com>
2022-07-12 20:27:19 +0530

    Updated production url and min url length

--- app/models/short_url_data.rb
+++ app/models/short_url_data.rb
@@ -1,5 +1,5 @@
 class ShortUrlData < ActiveRecord::Base
-  validates :real_url, presence: true, allow_nil: false, uniqueness: true, format: { with: URI::regexp(%w[http https]) }, length: { minimum: 28 }
+  validates :real_url, presence: true, allow_nil: false, uniqueness: true, format: { with: URI::regexp(%w[http https]) }, length: { minimum: 41 }
   validates :short_url, presence: true, allow_nil: false, uniqueness: true
 
   has_many :track_short_urls
